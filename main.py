da4a8b54b4cc2d9fe12cd7e1648f27ad41d5d466
Maita K <70974171+17mk1@users.noreply.github.com>
2022-02-15 16:43:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import arcade
+#import arcade
 import pygame
 import ballsort
+import random
 from ballsort import colour
 
 white = (255, 255, 255)
@@ -9,10 +10,9 @@ red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 colours=[red,green,blue]
-
+Ccolour=random.choice([red, green, blue])
 
 if __name__ == '__main__':
-
     #start game
     pygame.init()
 
@@ -28,44 +28,39 @@ if __name__ == '__main__':
     #setting up containers
 
     containters=[]
-    while i < 5:
+    for i in range(5):
         containters.append(ballsort.container)
-        containters[i].object = pygame.draw.rect(gameDisplay, white, (x, y, 50, 200))
-        #print(containters[i].object.x)
+        containters[i].object = pygame.draw.rect(gameDisplay, Ccolour , (x, y, 50, 200))
+        containters[i].colour=Ccolour
         x = x + 100
         i= i + 1
-
 
 
     #setting up balls
     x=125
     y=175
     a = 1
-
     containerNo = len(containters)
     colourList = ballsort.colour(colours, containerNo)
     balls = ballsort.balls(colourList)
     placement = ballsort.placement(containerNo, balls)
     cL=[]
-
-
+    ind=0
 
     for i in colourList:
-
         ballsort.ball = pygame.draw.circle(gameDisplay, i, (x, y), 15)
-        cL.append(ballsort.ball)
+        cL.append([ballsort.ball,i,ind])
         y=y+50
-
         if a%4 == 0 :
             x = x+100
             y = 175
         a=a+1
-
-    print(containters[1].object.x)
+        ind=ind+1
 
     rects=cL
     selected = None
 
+    count=4
     #quit game
     while True:
         for event in pygame.event.get():
@@ -77,14 +72,14 @@ if __name__ == '__main__':
                 if event.button == 1:
                     for i, r in enumerate(rects):
                         # Pythagoras a^2 + b^2 = c^2
-                        dx = r.centerx - event.pos[0]  # a
-                        dy = r.centery - event.pos[1]  # b
+                        dx = r[0].centerx - event.pos[0]  # a
+                        dy = r[0].centery - event.pos[1]  # b
                         distance_square = dx ** 2 + dy ** 2  # c^2
 
                         if distance_square <= 15 ** 2:  # c^2 <= radius^2
                             selected = i
-                            selected_offset_x = r.x - event.pos[0]
-                            selected_offset_y = r.y - event.pos[1]
+                            selected_offset_x = r[0].x - event.pos[0]
+                            selected_offset_y = r[0].y - event.pos[1]
 
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
@@ -93,18 +88,28 @@ if __name__ == '__main__':
             elif event.type == pygame.MOUSEMOTION:
                 if selected is not None:  # selected can be `0` so `is not None` is required
                     # move object
-                    rects[selected].x = event.pos[0] + selected_offset_x
-                    rects[selected].y = event.pos[1] + selected_offset_y
+                    rects[selected][0].x = event.pos[0] + selected_offset_x
+                    rects[selected][0].y = event.pos[1] + selected_offset_y
 
             gameDisplay.fill((0,0,0))
 
             for j in range(len(containters)):
-                pygame.draw.rect(gameDisplay, white, (containters[j].object.x, 150, 50, 200))
+                pygame.draw.rect(gameDisplay, Ccolour, (containters[j].object.x, 150, 50, 200))
 
             for i in range(len(colourList)):
-                pygame.draw.circle(gameDisplay,colourList[i], rects[i].center, 10)
+                pygame.draw.circle(gameDisplay,colourList[i], rects[i][0].center, 10)
 
 
+            for i in rects:
+                if ( i[0].x <=550 and i[0].x >=500 ) and (i[0].y >=150 and i[0].y<=350) and i[1]==Ccolour:
+                    ind = i[2]
+                    del colourList[ind]
+                    rects.remove(i)
+                    count=count-1
+                    selected=None
+        if count==0:
+                print("Winner!")
+                break
         pygame.display.update()
 
 
